e1861b9b9523e5f8175ac36231aa9cb479a814b6
creativeloc <creativeloc@yahoo.com>
2025-05-02 01:11:41 -0400

    convert static tours component into dynamic tours component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1Img from "./images/tour-1.jpeg"
+import tour2Img from "./images/tour-2.jpeg"
+import tour3Img from "./images/tour-3.jpeg"
+import tour4Img from "./images/tour-4.jpeg"
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -29,5 +34,52 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+  }
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1Img,
+    imgAlt: "Tibet Adventure",
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumqu vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100
+  },
+  {
+    id: 2,
+    image: tour2Img,
+    imgAlt: "best of java",
+    date: "july 7, 2021",
+    title: "best of java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumqu vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: 11,
+    cost: 1400
+  },
+  {
+    id: 3,
+    image: tour3Img,
+    imgAlt: "explore hong kong",
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumqu vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: 8,
+    cost: 5000
+  },
+  {
+    id: 4,
+    image: tour4Img,
+    imgAlt: "kenya highlights",
+    date: "november 18th, 2022",
+    title: "kenya highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumqu vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "kenya",
+    duration: 20,
+    cost: 3300
   }
 ]
