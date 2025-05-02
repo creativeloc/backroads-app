27e5b89cd4b29c5867cfb0b0ae1f98f2f0609822
Lawrence Caulker <locaulker@gmail.com>
2025-05-02 09:14:11 -0400

    convert static footer component links to dynamic links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,63 +1,45 @@
+import { pageLinks, socialLinks } from "../data"
+
 const Footer = () => {
   return (
     <footer className="section footer">
       <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
+        {pageLinks.map((link) => {
+          const { id, href, text } = link
+
+          return (
+            <li key={id}>
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          )
+        })}
       </ul>
+
       <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
+        {socialLinks.map((socialLink) => {
+          const { id, href, icon } = socialLink
+
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                className="footer-icon"
+                rel="noreferrer"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          )
+        })}
       </ul>
+
       <p className="copyright">
-        copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        Backroads travel tours company &mdash;
+        <span id="date">Copyright &copy; {new Date().getFullYear()}</span>{" "}
+        &mdash; all rights reserved
       </p>
     </footer>
   )
